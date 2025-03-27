f7e9ea5a35996d191c0d46755b62278135e1acc0
tinamanga <christinamanga28@gmail.com>
2025-03-27 20:51:13 +0300

    fetch lab

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json()) // Convert the response to JSON
+  .then((json) => {
+    // Call renderBooks() and pass the fetched data
+    renderBooks(json);
+  })
+  .catch((error) => {
+    console.error("Error fetching books:", error);
+  });
   
 }
 
